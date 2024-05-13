9733f5f32f3ce69bd7fcd8cab909b9bf13c81fea
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-13 15:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+	let p1=new Promise((a,b)=>{
+		resolve(images[0].url);
+	});
+	let p2=new Promise((a,b)=>{
+		resolve(images[1].url);
+	});
+	let p3=new Promise((a,b)=>{
+		resolve(images[2].url);
+	});
+function f1(p1,p2,p3) {
+	return [p1,p2,p3];
+}
+Promise.all(f1(p1,p2,p3)).then((d)=>{
+	output.innerHTML=`<img src='${d}' alt=''>`;
+})
